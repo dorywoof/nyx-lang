df81d02b5c5858ae30355fdf8de8a9dbae836b4d
Doruk <dorukmiril00@gmail.com>
2026-08-31 15:40:33 +0300

    Root test strings on the VM stack so the table suite survives GC stress

--- tests/test_table.c
+++ tests/test_table.c
@@ -3,16 +3,25 @@
 
 #include "nyx/object.h"
 #include "nyx/table.h"
+#include "nyx/vm.h"
 #include "test_harness.h"
+
+static ObjString *rootedString(const char *chars, int length) {
+    ObjString *string = copyString(chars, length);
+    push(OBJ_VAL(string));
+    return string;
+}
 
 void run_table_tests(void) {
     TEST_SUITE("table");
 
+    Value *savedStackTop = vm.stackTop;
+
     Table table;
     initTable(&table);
 
-    ObjString *key1 = copyString("foo", 3);
-    ObjString *key2 = copyString("bar", 3);
+    ObjString *key1 = rootedString("foo", 3);
+    ObjString *key2 = rootedString("bar", 3);
 
     Value out;
     TEST_CHECK(!tableGet(&table, key1, &out));
@@ -38,22 +47,24 @@ void run_table_tests(void) {
     char buf[16];
     for (int i = 0; i < 200; i++) {
         snprintf(buf, sizeof(buf), "key%d", i);
-        ObjString *k = copyString(buf, (int)strlen(buf));
+        ObjString *k = rootedString(buf, (int)strlen(buf));
         tableSet(&big, k, NUMBER_VAL(i));
     }
     TEST_CHECK_EQ_INT(big.count, 200);
     for (int i = 0; i < 200; i++) {
         snprintf(buf, sizeof(buf), "key%d", i);
-        ObjString *k = copyString(buf, (int)strlen(buf));
+        ObjString *k = rootedString(buf, (int)strlen(buf));
         Value v;
         TEST_CHECK(tableGet(&big, k, &v));
         TEST_CHECK(AS_NUMBER(v) == i);
     }
 
-    ObjString *a = copyString("interned", 8);
-    ObjString *b = copyString("interned", 8);
+    ObjString *a = rootedString("interned", 8);
+    ObjString *b = rootedString("interned", 8);
     TEST_CHECK(a == b);
 
     freeTable(&table);
     freeTable(&big);
+
+    vm.stackTop = savedStackTop;
 }
